8b054e9639d57853c0bb2208e91596d6699b2c6b
Gani Parrott <gparrott@ucsc.du>
2017-05-09 13:46:42 -0700

    audio components are written. moving on to political spectrum

---
+++ Assets/Prototype/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+// controls the player's movement and ensures that it doesn't penetrate any surfaces we'd like to assume are solid
+public class PlayerController : MonoBehaviour
+{
+    // teh speed at which the player moves
+    public float _moveSpeed = 10f;
+
+    // the speed at which the player rotates
+    public float _rotationSpeed = 10f;
+
+    // the name of the axis from which we will infer the amount of movement in the horizontal direction
+    public string _leftRightAxis = "horizontal";
+
+    // the name of the axis from which we will infer the amount of movement in the forward backward direction
+    public string _forwardBackAxis = "vertical";
+
+    // the name of the axis from which we will infer the rotation direction
+    public string _rotationAxis = "turning";
+
+    // the rigidbody that we assume is attached to the player which takes care of things like moving through walls
+    private Rigidbody _rigidbody;
+    
+    private void Start()
+    {
+        // get the Rigidbody object from the GameObject this script (or component) is attached to
+        // so that we may interface with it to deal with physics calculations without much stress
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // get the values from the axes
+        // for more information about this, see https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
+        float horizontalAmount = Input.GetAxis(_leftRightAxis);
+        float verticalAmount = Input.GetAxis(_forwardBackAxis);
+        float rotationAmount = Input.GetAxis(_rotationAxis);
+
+        // compute the displacement from the axes
+        Vector3 horizontalDisplacement = Vector3.one * horizontalAmount * _moveSpeed * Time.deltaTime;
+        Vector3 verticalDisplacement = Vector3.one * verticalAmount * _moveSpeed * Time.deltaTime;
+
+        // compute the rotation from the axis
+        Quaternion rotation = Quaternion.AngleAxis(rotationAmount * _rotationSpeed * Time.deltaTime, Vector3.up);
+
+        // compute the desired next position of the player
+        Vector3 nextPosition = transform.position + horizontalDisplacement + verticalDisplacement;
+
+        // use the rigidbody to make sure the desired position is physically possible to go to
+        _rigidbody.MovePosition(nextPosition);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
---
+++ Assets/Prototype/Scripts/TheVoice.cs
@@ -0,0 +1,128 @@
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[System.Serializable]
+public struct TaggedVoiceLine
+{
+    public string _uid;
+
+    public AudioClip _audioClip;
+
+    public List<string> _tags;
+}
+
+[System.Serializable]
+public struct InputAxisTagPair
+{
+    public string _inputAxis;
+
+    public string _tag;
+}
+
+// ensure the GameObject this script is attached to has an AudioSource component
+[RequireComponent(typeof(AudioSource))]
+public class TheVoice : MonoBehaviour
+{
+    public List<TaggedVoiceLine> _voiceLines = new List<TaggedVoiceLine>();
+
+    public List<InputAxisTagPair> _axisTagPairs = new List<InputAxisTagPair>();
+
+    private Dictionary<string, string> _axisTagMap = new Dictionary<string, string>();
+
+    private Dictionary<string, TaggedVoiceLine> _idVoiceLineMap = new Dictionary<string, TaggedVoiceLine>();
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        // setup audio source
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.loop = false;
+
+        // make sure there's something in the _voiceLines list
+        if (_voiceLines.Count == 0)
+        {
+            Debug.Log("TheVoice warning: There are no TaggedVoiceLine objects added to this component. This script will break");
+        }
+
+        // make sure there's something in the _axisTagPairs list
+        if (_axisTagPairs.Count == 0)
+        {
+            Debug.Log("TheVoice warning: There are no InputAxisTagPair(s) added to this component. This script will break");
+        }
+
+        // add voice lines to dictionary
+        foreach (var line in _voiceLines)
+        {
+            _idVoiceLineMap.Add(line._uid, line);
+        }
+
+        // add axis tag pairs to the private dictionary
+        foreach (var pair in _axisTagPairs)
+        {
+            _axisTagMap.Add(pair._inputAxis, pair._tag);
+        }
+    }
+
+    private void Update()
+    {
+        // find the input axis that was pressed and play a random voice line with a matching tag
+        foreach (var axisTagPair in _axisTagPairs)
+        {
+            float axisValue = Input.GetAxis(axisTagPair._inputAxis);
+
+            if (axisValue != 0)
+            {
+                _audioSource.clip = SelectAudioClipByTag(axisTagPair._tag);
+            }
+        }
+        _audioSource.Play();
+    }
+
+    // select a random clip from the list of clips with the given tag
+    private AudioClip SelectAudioClipByTag(string tag)
+    {
+        List<AudioClip> clipsWithTag = GetAudioClipsByTag(tag);
+
+        int randomIndex = Random.Range(0, clipsWithTag.Count);
+
+        return clipsWithTag[randomIndex];
+    }
+
+    private List<TaggedVoiceLine> GetVoiceLinesWithTag(string tag)
+    {
+        List<TaggedVoiceLine> toReturn = new List<TaggedVoiceLine>();
+        foreach (var line in _voiceLines)
+        {
+            if (line._tags.Contains(tag))
+            {
+                toReturn.Add(line);
+            }
+        }
+        return toReturn;
+    }
+
+    private List<AudioClip> GetAudioClipsByTag(string tag)
+    {
+        List<AudioClip> toReturn = new List<AudioClip>();
+        foreach (var line in _voiceLines)
+        {
+            if (line._tags.Contains(tag))
+            {
+                toReturn.Add(line._audioClip);
+            }
+        }
+        return toReturn;
+    }
+
+    private TaggedVoiceLine GetVoiceLineByUID(string uid)
+    {
+        return _idVoiceLineMap[uid];
+    }
+
+    private AudioClip GetAudioClipByUID(string uid)
+    {
+        return _idVoiceLineMap[uid]._audioClip;
+    }
+}
