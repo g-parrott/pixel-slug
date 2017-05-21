4f82427831112b8308b6d969f94338cf106a6035
Dominique DeJarnatt <Meta@eduroam-169-233-201-185.ucsc.edu>
2017-05-20 18:06:18 -0700

    added a new prototype with camera tweaks and better wall collision detection

--- Assets/Prototype/Scripts/EngineControllers/PlayerController.cs
+++ Assets/Prototype/Scripts/EngineControllers/PlayerController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 // controls the player's movement and ensures that it doesn't penetrate any surfaces we'd like to assume are solid
 public class PlayerController : MonoBehaviour
 {
-    // teh speed at which the player moves
+    // the speed at which the player moves
     public float _moveSpeed = 10f;
 
     // the speed at which the player rotates
@@ -28,7 +28,7 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // get the values from the axes
         // for more information about this, see https://docs.unity3d.com/ScriptReference/Input.GetAxis.html
@@ -37,8 +37,8 @@ public class PlayerController : MonoBehaviour
         float rotationAmount = Input.GetAxis(_rotationAxis);
 
         // compute the displacement from the axes
-        Vector3 horizontalDisplacement = Vector3.right * horizontalAmount * _moveSpeed * Time.deltaTime;
-        Vector3 verticalDisplacement = Vector3.forward * verticalAmount * _moveSpeed * Time.deltaTime;
+        Vector3 horizontalDisplacement = Vector3.right * horizontalAmount * _moveSpeed * Time.fixedDeltaTime;
+        Vector3 verticalDisplacement = Vector3.forward * verticalAmount * _moveSpeed * Time.fixedDeltaTime;
 
         // Bugged for now
         // compute the rotation from the axis
@@ -51,8 +51,20 @@ public class PlayerController : MonoBehaviour
         // compute the desired next position of the player
         Vector3 nextPosition = transform.position + horizontalDisplacement + verticalDisplacement;
 
+		// check if player would go through a wall and if so don't go through the wall
+		foreach (var hit in Physics.RaycastAll(transform.position, (horizontalDisplacement + verticalDisplacement).normalized)) {
+			if (hit.transform.tag == "Level") {
+				var point = hit.point;
+				var distance = Vector3.Distance (transform.position, point);
+				if (distance < 1) {
+					nextPosition = transform.position;
+				}
+			}
+		}
+
         // use the rigidbody to make sure the desired position is physically possible to go to
-        _rigidbody.MovePosition(nextPosition);
+		transform.position = nextPosition;
+		//_rigidbody.MovePosition(nextPosition);
 
         //_rigidbody.MoveRotation(rotation);
     }
