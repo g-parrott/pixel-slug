7a74ba54e935b63c3bb71b16a66b29fe089e9b4c
whimstasy <acwinter@ucsc.edu>
2017-05-30 19:29:08 -0700

    Better environment updated as well as adding the afro peep

---
+++ Assets/Prototype/Scripts/EngineControllers/AfroWalkWay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AfroWalkWay : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start () {
+		//give reference to our agent
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//tell our agent where to go
+		agent.SetDestination(target.position);
+	}
+}
